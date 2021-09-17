a3905f34a8fc4c18cc2d6be9266a3c410f6e2a3d
pedrozebra <pdellanotte@gmail.com>
2021-09-17 12:15:27 +0200

    Updated config

--- helpers/config.js
+++ helpers/config.js
@@ -1,5 +1,6 @@
 export const postUrl = "https://www.pietrodellanotte.dev/posts/";
 export const siteImagesUrl = "https://www.pietrodellanotte.dev/images/posts/";
 export const name = 'Pietro Della Notte'
-export const siteTitle = 'Pietro Della Notte developer'
+export const siteTitle = 'Pietro Della Notte Developer'
+export const siteTitleAbout = 'Pietro Della Notte Developer | About'
 export const siteName = 'pietrodellanotte.dev';
