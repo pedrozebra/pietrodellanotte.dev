65d2a431a68bfa509863fc2fcda7e29bd4883577
pedrozebra <pdellanotte@gmail.com>
2021-09-17 12:14:25 +0200

    Updated about page

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,18 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
-import { siteTitle } from "../helpers/config";
+import { siteTitleAbout } from "../helpers/config";
 import utilStyles from "../styles/utils.module.css";
 export default function About() {
     return (
         <Layout>
       <Head>
-        <title>{siteTitle}</title>
+        <title>{siteTitleAbout}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Ciao! <br />Sono Pietro Della Notte e da 8 anni lavoro come sviluppatore web. Per i primi 6 mi sono occupato di stack LAMP (PHP/MySQL), con uno sguardo anche al vecchio mondo FE (JQuery & co).</p>
-        <p>Da due anni lavoro come sviluppatore BE e Technical Leader su piattaforma Salesforce Commerce Cloud.<br/>
-        Al momento sono impegnato su progetti e-commerce di alta moda, una novità assoluta per me.
+        <p>Ciao! <br />Sono Pietro Della Notte, sono di Battipaglia, e dal 2013, dopo la Laurea triennale in Informatica, lavoro come sviluppatore web. <br/> <br/>
+        Dal Novembre 2013 all'Agosto 2019 ho lavorato presso l'azienda <a target="_blank" href="https://contactsud.it/">Contact Centre Sud</a> mi sono occupato prevalentemente di stack LAMP (PHP/MySQL), con uno sguardo anche al vecchio mondo FE (JQuery & co).</p><br/>
+        <p>Dal Settembre 2019 lavoro (full-remote) presso l'azienda <a target="_blank" href="https://alpenite.com/">alpenite</a> come sviluppatore BE e Technical Leader su piattaforma Salesforce Commerce Cloud.<br/>
+        Al momento sono impegnato su progetti e-commerce di alta moda & luxury, una novità assoluta per me.
         </p>
         <p>Sono aperto alla collaborazione, e ad imparare nuove tecnologie, se ti va, restiamo in contatto!</p>
         <p><a href="mailto:pdellanotte@gmail.com">Questa</a> è la mia mail (pdellanotte@gmail.com)</p> 
